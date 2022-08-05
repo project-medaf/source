b6e16d537fb8e802a9835405c2d31c94e086b519
anokidev <anoki.youssou@gmail.com>
2022-08-05 12:00:50 +0700

    rewrite

--- examples/web_project/project.py
+++ examples/web_project/project.py
@@ -11,12 +11,18 @@ PLUGIN_APPS: Configuring installed plugin apps in your project.
 
 There are 2 types of apps:
 
-- local: Those apps inside your project directory.
-- plugin: Those apps inside a python package that you installed.
+Based on the location:
+
+- Local app: These type of apps are located inside your project directory.
+- Plugin app: Those type of apps are located inside a python package that you installed.
 
 Based on the functionality:
 
-- content: Those apps
+- Content app: Those apps are the one that are going to handle the "content". For example: On a web project, a content app are going to create response, such as an HTTP response.
+
+- Middleware app: These apps have the ability to modify data sended from content apps to gateway apps and vice versa. For example: On a web project, a middleware app can modify requests and responses.
+
+- Gateway app: These apps are the "gateway". For example: On a web project, a gateway app will accept request from a WSGI server. Gateway apps can also send data (For a web project, a gateway app is going to send response to the WSGI server).
 
 -------------------------------
 
@@ -26,15 +32,14 @@ Medaf is licensed in MIT License, see ./LICENSE.md for more info.
 """
 
 # Local apps inside your project.
-LOCAL_APPS = {
-    'CONTENT':['my_app'],
-    'MIDDLEWARE':[],
-    'GATEWAY':[]
-}
+LOCAL_CONTENT_APPS    = ['my_apps']
+LOCAL_MIDDLEWARE_APPS = []
+LOCAL_GATEWAY_APPS    = []
 
 # Plugin apps inside your project.
-PLUGIN_APPS = {
-    'CONTENT':[],
-    'MIDDLEWARE':[],
-    'GATEWAY':[]
-}
+PLUGIN_CONTENT_APPS    = []
+PLUGIN_MIDDLEWARE_APPS = []
+PLUGIN_GATEWAT_APPS    = []
+
+# Debug mode.
+DEBUG_MODE = True
